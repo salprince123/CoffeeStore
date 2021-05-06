c2b8be35c47495c2ee95f8b74d55d063cd0b3c08
Dieu Le <18520265@gm.uit.edu.vn>
2021-05-06 11:59:04 +0700

    load inventoryImport from database

--- CoffeeStore/Inventory/InventoryMainPage.xaml.cs
+++ CoffeeStore/Inventory/InventoryMainPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CoffeeStore.BUS;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,35 @@ namespace CoffeeStore.Inventory
     /// </summary>
     public partial class InventoryMainPage : UserControl
     {
+        public class InventoryObject
+        {
+            public int number { get; set; }
+            public String Name { get; set; }
+            public String Unit { get; set; }
+            public String Amount { get; set; }
+            //this variable for button add/del/edit
+            public String Action { get; set; }
+        }
         public InventoryMainPage()
         {
             InitializeComponent();
+            LoadData();
+        }
+        public void LoadData()
+        {
+            /*var list = new ObservableCollection<InventoryObject>();
+            BUS_InventoryImport import = new BUS_InventoryImport();
+            DataTable temp = import.selectAll();
+            int number0 = 1;
+            Console.WriteLine(temp.Rows.Count);
+            foreach (DataRow row in temp.Rows)
+            {
+                string employid = row["EmployeeID"].ToString();
+                string id = row["importID"].ToString();
+                string date = row["importDate"].ToString();
+                //list.Add(new InventoryObject() { ID = id, EmployName = employid, InventoryDate = date, number = number0, Action = "" });
+            }
+            this.dataGridInfo.ItemsSource = list;*/
         }
     }
 }
