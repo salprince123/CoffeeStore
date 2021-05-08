b6df50d64a06f0b1aa02b79cc64a2f17a5c7db18
TranThiHaPhuong <59615434+TTph18@users.noreply.github.com>
2021-05-08 18:54:16 +0700

    Add black screen effect when open popup

--- CoffeeStore/Inventory/InventoryMainPage.xaml.cs
+++ CoffeeStore/Inventory/InventoryMainPage.xaml.cs
@@ -107,17 +107,22 @@ namespace CoffeeStore.Inventory
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
+            System.Windows.Media.Effects.BlurEffect objBlur = new System.Windows.Media.Effects.BlurEffect();
+            ((MainWindow)App.Current.MainWindow).Opacity = 0.5;
+            ((MainWindow)App.Current.MainWindow).Effect = objBlur;
             Window window = new Window
             {
-                Title = "Thêm vật liệu",
+                Title = "Thêm nguyên liệu, vật liệu",
                 Content = new PopupAddMaterial(),
                 Width=540,
-                Height=300,
-                Left= (Application.Current.MainWindow.Left+ Application.Current.MainWindow.Width -540/2)/2,
-                Top = (Application.Current.MainWindow.Top + Application.Current.MainWindow.Height -300/2) / 2,
+                Height=280,
+                Left= (Application.Current.MainWindow.Left+ Application.Current.MainWindow.Width - 900/2)/2,
+                Top = (Application.Current.MainWindow.Top + Application.Current.MainWindow.Height -600/2) / 2,
             };
             window.ShowDialog();
             LoadData();
+            ((MainWindow)App.Current.MainWindow).Opacity = 1;
+            ((MainWindow)App.Current.MainWindow).Effect = null;
         }
 
         private void btnEdit_Click(object sender, RoutedEventArgs e)
@@ -127,17 +132,22 @@ namespace CoffeeStore.Inventory
                 InventoryObject row = (InventoryObject)dataGridInfo.SelectedItem;
                 if(row != null)
                 {
+                    System.Windows.Media.Effects.BlurEffect objBlur = new System.Windows.Media.Effects.BlurEffect();
+                    ((MainWindow)App.Current.MainWindow).Opacity = 0.5;
+                    ((MainWindow)App.Current.MainWindow).Effect = objBlur;
                     Window window = new Window
                     {
-                        Title = "Sửa vật liệu",
+                        Title = "Sửa nguyên liệu, vật liệu",
                         Content = new PopupEditMaterial(row.Name, row.Unit),
                         Width = 540,
                         Height = 300,
-                        Left = (Application.Current.MainWindow.Left + Application.Current.MainWindow.Width - 540 / 2) / 2,
-                        Top = (Application.Current.MainWindow.Top + Application.Current.MainWindow.Height - 300 / 2) / 2,
+                        Left = (Application.Current.MainWindow.Left + Application.Current.MainWindow.Width - 900 / 2) / 2,
+                        Top = (Application.Current.MainWindow.Top + Application.Current.MainWindow.Height - 600 / 2) / 2,
                     };
                     window.ShowDialog();
                     LoadData();
+                    ((MainWindow)App.Current.MainWindow).Opacity = 1;
+                    ((MainWindow)App.Current.MainWindow).Effect = null;
                 }
                 
             }
