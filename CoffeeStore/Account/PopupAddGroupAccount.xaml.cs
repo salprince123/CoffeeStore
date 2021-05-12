456a5c019630269a019e4315782a47194759e824
Dieu Le <18520265@gm.uit.edu.vn>
2021-05-12 21:52:13 +0700

    small fix

--- CoffeeStore/Account/PopupAddGroupAccount.xaml.cs
+++ CoffeeStore/Account/PopupAddGroupAccount.xaml.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD:CoffeeStore/Account/PopupAddGroupAccount.xaml.cs
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,54 +26,3 @@ namespace CoffeeStore.Account
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace CoffeeStore
-{
-    /// <summary>
-    /// Interaction logic for MainMenu.xaml
-    /// </summary>
-    public partial class MainMenu : UserControl
-    {
-        public MainMenu()
-        {
-            InitializeComponent();
-            this.DataContext = this;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnEdit_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void dgMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-    }
-}
->>>>>>> StoVer6:CoffeeStore/MainMenu.xaml.cs
