71df3fc03d27e82f009fe7affd1b16f45eacbee3
Châu Trần Lê Bảo <18520256@ms.uit.edu.vn>
2021-05-13 17:04:31 +0700

    Add some contraint and data to database, add some DTO class for accounts and employees management

---
+++ CoffeeStore/DTO/DTO_EmployeeType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeStore.DTO
+{
+    class DTO_EmployeeType
+    {
+        #region Atrributes
+        private string EmployeeTypeID;
+        private string EmployeeTypeName;
+        #endregion
+
+        #region Method
+        DTO_EmployeeType()
+        {
+            EmployeeTypeID = "";
+            EmployeeTypeName = "";
+        }
+        DTO_EmployeeType(string ID, string name)
+        {
+            EmployeeTypeID = ID;
+            EmployeeTypeName = name;
+        }
+        #region GetSetMethod
+        string GetEmployeeID()
+        {
+            return EmployeeTypeID;
+        }
+        string GetEmployeeTypeName()
+        {
+            return EmployeeTypeName;
+        }
+        void SetEmployeeTypeID(string newTypeId)
+        {
+            EmployeeTypeID = newTypeId;
+        }
+        void SetEmployeeTypeName(string newName)
+        {
+            EmployeeTypeName = newName;
+        }
+        #endregion
+        #endregion
+    }
+}
