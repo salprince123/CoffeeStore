9ccbfd88ef787bb01e81df1488cdce7c418da063
TranThiHaPhuong <59615434+TTph18@users.noreply.github.com>
2021-05-16 17:38:43 +0700

    Add Popup Discount and Payment

---
+++ CoffeeStore/IncomeAndPayment/PopupPaymentAdd.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoffeeStore.IncomeAndPayment
+{
+    /// <summary>
+    /// Interaction logic for PopupPaymentAdd.xaml
+    /// </summary>
+    public partial class PopupPaymentAdd : UserControl
+    {
+        public PopupPaymentAdd()
+        {
+            InitializeComponent();
+        }
+    }
+}
