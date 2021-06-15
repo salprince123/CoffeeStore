cbce250da173626421c4cdab42b1373d06e4b939
Cúc Phan Thị Hồng <18520260@ms.uit.edu.vn>
2021-06-15 23:02:31 +0700

    Add Increment Number for DiscountList

--- CoffeeStore/Discount/DiscountList.xaml.cs
+++ CoffeeStore/Discount/DiscountList.xaml.cs
@@ -32,7 +32,13 @@ namespace CoffeeStore.Discount
             //dgDiscount.ItemsSource = bus.getAllDiscount().DefaultView;
             else
                 MessageBox.Show("Null");*/
+            dgDiscount.LoadingRow += new EventHandler<DataGridRowEventArgs>(datagrid_LoadingRow);
             loadData();
+        }
+        void datagrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = e.Row.GetIndex() + 1;
+            e.Row.Height = 40;
         }
         void loadData()
         {
