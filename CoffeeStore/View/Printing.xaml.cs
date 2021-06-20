1b9bde179f566cacafaf33526a834bf73f083e1d
TranThiHaPhuong <59615434+TTph18@users.noreply.github.com>
2021-06-20 21:44:51 +0700

    Update Cashier/Print

--- CoffeeStore/View/Printing.xaml.cs
+++ CoffeeStore/View/Printing.xaml.cs
@@ -24,5 +24,10 @@ namespace CoffeeStore.View
         {
             InitializeComponent();
         }
+
+        private void btnPrint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
