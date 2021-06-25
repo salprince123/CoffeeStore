06d3a7e0736b8f150f603db1801925347b444844
Cúc Phan Thị Hồng <18520260@ms.uit.edu.vn>
2021-06-25 22:07:31 +0700

    Add Rule & RuleEdit

---
+++ CoffeeStore/Rule/Rule.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoffeeStore.Rule
+{
+    /// <summary>
+    /// Interaction logic for Rule.xaml
+    /// </summary>
+    public partial class Rule : UserControl
+    {
+        public Rule()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
